6520dd56052f71098ab33a15f2750b43ed13ef70
Sébastien <sebastien.kovacs.ext@martingmbh.de>
2023-09-19 18:01:12 +0200

    pt2

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,38 @@
+//use std::thread::spawn;
+
 use bevy::prelude::*;
+use bevy::render::camera::ScalingMode;
+
+#[derive(Component)]
+pub struct Player {
+    pub speed: f32,
+}
+
+#[derive(Component)]
+pub struct Pig {
+    pub lifetime: Timer,
+}
+
+#[derive(Resource)]
+pub struct Money(pub f32);
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
+    let mut camera = Camera2dBundle::default();
+    camera.projection.scaling_mode = ScalingMode::AutoMin {
+        min_width: 256.0,
+        min_height: 144.0,
+    };
+    commands.spawn(camera);
 
     let texture = asset_server.load("character.png");
 
-    commands.spawn(SpriteBundle {
-        sprite: Sprite {
-            custom_size: Some(Vec2::new(100.0, 100.0)),
+    commands.spawn((
+        SpriteBundle {
+            texture,
             ..default()
         },
-        texture,
-        ..default()
-    });
+        Player { speed: 100.0 }
+    ));
 }
 
 fn main() {
@@ -31,29 +51,92 @@ fn main() {
                 })
                 .build(),
         )
+        .insert_resource(Money(100.0))
         .add_systems(Startup, setup)
-        .add_systems(Update, character_movement)
+        .add_systems(Update, (character_movement, spawn_pig, pig_lifetime, pig_movement))
         .run();
 }
 
 
 fn character_movement(
-    mut characters: Query<(&mut Transform, &Sprite)>,
+    mut characters: Query<(&mut Transform, &Player)>,
     input: Res<Input<KeyCode>>,
     time: Res<Time>,
 ) {
-    for (mut transform, _) in &mut characters {
+    for (mut transform, player) in &mut characters {
+        let movement_amount = player.speed * time.delta_seconds();
         if input.pressed(KeyCode::W) {
-            transform.translation.y += 100.0 * time.delta_seconds();
+            transform.translation.y += movement_amount;
         }
         if input.pressed(KeyCode::S) {
-            transform.translation.y -= 100.0 * time.delta_seconds();
+            transform.translation.y -= movement_amount;
         }
         if input.pressed(KeyCode::D) {
-            transform.translation.x += 100.0 * time.delta_seconds();
+            transform.translation.x += movement_amount;
         }
         if input.pressed(KeyCode::A) {
-            transform.translation.x -= 100.0 * time.delta_seconds();
+            transform.translation.x -= movement_amount;
         }
+    }
+}
+
+fn spawn_pig(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    input: Res<Input<KeyCode>>,
+    mut money: ResMut<Money>,
+    player: Query<&Transform, With<Player>>,
+) {
+    if !input.just_pressed(KeyCode::Space) {
+        return;
+    }
+
+    let player_transform = player.single();
+
+    if money.0 >= 10.0 {
+        money.0 -= 10.0;
+        info!("Spent $10 on a pig, remaining money: ${:?}", money.0);
+
+        let texture = asset_server.load("pig.png");
+
+        commands.spawn((
+            SpriteBundle {
+                texture,
+                transform: *player_transform,
+                ..default()
+            },
+            Pig {
+                lifetime: Timer::from_seconds(10.0, TimerMode::Once),
+            },
+        ));
+    }
+}
+
+fn pig_lifetime(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut pigs: Query<(Entity, &mut Pig)>,
+    mut money: ResMut<Money>,
+) {
+    for (pig_entity, mut pig) in &mut pigs {
+        pig.lifetime.tick(time.delta());
+
+        if pig.lifetime.finished() {
+            money.0 += 15.0;
+
+            commands.entity(pig_entity).despawn();
+
+            info!("Pig sold for $15! Current Money: ${:?}", money.0);
+        }
+    }
+}
+
+fn pig_movement(
+    mut pigs: Query<(&mut Transform, &Pig)>,
+    time: Res<Time>,
+) {
+    let movement_amount = 10.0 * time.delta_seconds();
+    for (mut transform, _) in &mut pigs {
+        transform.translation.x += movement_amount;
     }
 }
